a24a33a655b0281f4deeb3f9a0e3784b5e682cd7
SamuCodev <samuelddavid0530@gmail.com>
2026-08-24 22:19:10 -0500

    Feat: Funcion de App React PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+    plugins: [
+        react(),
+        babel({ presets: [reactCompilerPreset()] }),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "CuidaCasa",
+                short_name: "CuidaCasa",
+                description: "El cuidado de tu hogar en un solo lugar",
+                theme_color: "hsl(193, 54%, 23%)",
+                background_color: "hsl(36, 33%, 97%)",
+                display: "standalone",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "/home-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/home-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
